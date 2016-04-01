f6d3c2bd005b5ed163c5e3ed2b4da5bd06778f83
Mathew WRIGHT <mwrigh22@trusty>
2016-04-01 16:36:21 -0400

    Make login page and started to set up C program to verify user credentials

---
+++ cgi-bin/login.c
@@ -0,0 +1,15 @@
+# include <stdlib.h>
+# include <stdio.h>
+
+int main(){
+	/*char *data = getenv("QUERY_STRING");
+	char name[100], username[100], jobDescription[250];
+	sscanf(data, "%s: %s, %s", username, name, jobDescription);
+	printf("%s\n%s\n%s", username, name, jobDescription);*/
+/*	printf("Content-Type: text/plain;charset=utf-8\n\n");
+	printf("\n");
+  	printf("Hello world\n\n");*/
+    printf("Content-type: text/html\n\n");
+    printf("Hi from the CGI!\n");
+	return 0;
+}
